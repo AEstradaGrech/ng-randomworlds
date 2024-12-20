8a3bccacbad3a1d0d875acc64278dff7cea98d77
Alvaro Estrada <alvaro.estrada89@gmail.com>
2024-12-20 18:42:15 +0100

    [quest-view-comp] --> Central card with lateral expandable panels OK (open/close OK)

--- src/app/modules/bussiness/pages/quest-view/quest-view.component.ts
+++ src/app/modules/bussiness/pages/quest-view/quest-view.component.ts
@@ -1,13 +1,40 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quest-view',
   templateUrl: './quest-view.component.html',
-  styleUrl: './quest-view.component.scss'
+  styleUrl: './quest-view.component.scss',
+  animations: [
+    trigger('panelStateL', [
+      state('visible', style({ width: '*', opacity: 1 })),
+      state('hidden', style({ width: '0', opacity: 0, overflow: 'hidden' })),
+      transition('* => visible', animate('300ms ease-in')),
+      transition('* => hidden', animate('300ms ease-out'))
+    ]),
+    trigger('panelStateR', [
+      state('visible', style({ width: '*', opacity: 1 })),
+      state('hidden', style({ width: '0', opacity: 0, overflow: 'hidden' })),
+      transition('* => visible', animate('300ms ease-in')),
+      transition('* => hidden', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class QuestViewComponent {
 
+  panelStateR = 'hidden';
+  panelStateL = 'hidden';
   public onSubmit(){
     console.log('-- on submit --')
+  }
+
+  onOpenBtnClick(btn:string){
+    if(btn === 'left'){
+      console.log('-- on left click --')
+      this.panelStateL = (this.panelStateL === 'visible') ? 'hidden' : 'visible';
+    }else{
+      console.log('-- on right btn click --')
+      this.panelStateR = (this.panelStateR === 'visible') ? 'hidden' : 'visible';
+    }
   }
 }
