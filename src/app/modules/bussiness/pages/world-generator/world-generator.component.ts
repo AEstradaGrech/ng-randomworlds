f058cb7a8c4c87597ca6b18a81708e095100acf6
Alvaro Estrada <alvaro.estrada89@gmail.com>
2024-12-20 22:27:32 +0100

    [world-generator-comp] --> adding form and updating UI

--- src/app/modules/bussiness/pages/world-generator/world-generator.component.ts
+++ src/app/modules/bussiness/pages/world-generator/world-generator.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router'
+
 @Component({
   selector: 'app-world-generator',
   templateUrl: './world-generator.component.html',
@@ -9,9 +11,11 @@ import { Router } from '@angular/router'
 export class WorldGeneratorComponent implements OnInit{
   public gameType!: string | null;
   public charName!: string | null;
-  
-  private _router = inject(Router)
-  private _snackBar = inject(MatSnackBar)
+  public form!: FormGroup;  
+  private _fb:FormBuilder = inject(FormBuilder);
+  private _router = inject(Router);
+  private _snackBar = inject(MatSnackBar);
+
   ngOnInit(): void {
     this.gameType = localStorage.getItem('game-type');
     this.charName = localStorage.getItem('selected-char');
@@ -20,6 +24,12 @@ export class WorldGeneratorComponent implements OnInit{
       this._snackBar.open("An error has occured while trying to begin the game. Game type or Character bad configured", undefined, { duration: 2500,panelClass: ['snack-warning'], verticalPosition: 'bottom'})
       this._router.navigateByUrl('randomworlds/home')
     }
+
+    this.form = this._fb.group({
+      ambience: new FormControl('Fantasy, Medieval'),
+      genres: new FormControl('Thriller, Survival, Suspense'),
+      plot: new FormControl(undefined, [Validators.maxLength(200)])
+    })
   }
   public onGenerateClick(){
     console.log('-- onGenerate --')
