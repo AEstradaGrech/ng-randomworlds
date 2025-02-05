7778ab8c0e664ac911d1a11b779b9cacc682a18d
Alvaro Estrada <alvaro.estrada89@gmail.com>
2025-02-06 00:40:00 +0100

    [marketplace-comp] --> displaying factory-catalogue models. styling (provisional) UI

--- src/app/modules/bussiness/services/smart-contracts.service.ts
+++ src/app/modules/bussiness/services/smart-contracts.service.ts
@@ -6,7 +6,7 @@ import Factory from 'src/app/core/scripts/immutableFactory'
 import Collection from 'src/app/core/scripts/immutableCollection'
 import Decrypter from 'src/app/core/scripts/profileDecrypt'
 import { firstValueFrom } from 'rxjs';
-import { CatalogueCollection, CharacterProfile, CollectionSummary, ModelInfo } from 'src/app/core/interfaces/business/smart-contract.interface';
+import { CatalogueCollection, CharacterMetadata, CharacterProfile, CollectionSummary, ModelInfo } from 'src/app/core/interfaces/business/smart-contract.interface';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -84,20 +84,21 @@ export class SmartContractsService {
     }
     return info;
   }
-  public async getModelMetadata(model:ModelInfo, collection: CollectionSummary, contract:any) : Promise<any>{
+  public async getModelMetadata(model:ModelInfo, collection: CollectionSummary) : Promise<any>{
     let url = `${collection.gateway}/${collection.metaCid}/${model.fileName}.json`;
     console.log('meta url', url);
-    let rawMetadata = await firstValueFrom(this.http.get<any>(url));
-    console.log('meta resp', rawMetadata);
-    let testEncryption = 'U2FsdGVkX18b/eiGmtdX/+wF9zjwoK5+0GOZiPV/iT3EyWCOewqBLivJxUJBNQDAKffDlHOmeJl+5DU1Mgl/Xd6Y94r3leWCAaTRCA7CCGeSQGQ7ZHSrp9LSF770BEGi7CedpiElyOBbZE47AYQLMv+qYL8NtmVbpOeBSzUQpeXWsBVCbvbWkuWNIxB3HETEYmadMqUEKL/bFYADBnj8zjEGjsX6PEyWhgQqJP5lP52syVPubR8KBSRqQemLEftZUm72k4e2/Nuw6T1aMJjgDyBeDsGxEJ8hJfIJNLboabV7JELJ+31/MY5JG5MRT2ylU+lX7v16AutSFiduuNSJZd1nW9Xk0tH97yeahq0Wrx2Q8ov1cVRMICLkk9vyTVrWbnI/ldi9DKE6IfCk0pe2XYb5wyEVSW0XekBP489alDQ7R56+jeKiZBrjEhXVc3JNY6oqGTl/+dutxeWazxlUHcCTHRdJFjOcfBzx/fpOJAjlplPVMAYK7uJC0ZcOQbgDnWGhzIa97niAqD+X39Prrrv63UNpJp4f0wA4XD+h35cxe/ohMdxrWkIGPpT84VMSUzHpxuReIvS/C1UjG+6twbZss7y+QfhSl8tuAox0zzLAmwwU8ygb3EOsahVd1OgDkjTEm5UrXmK1rI6cA1oIZ+N+x/3Zc1KNG8MdYTRZF73u7DB0rBZLU1PCXHixloieWv4E1LPe7kWUxVe27cTUnI0HxNwwkyWBKicXwoYSCsHa7F/v2LqaKuDD9QmN6lqpw+98i9maLdS2fzEvk2dSNbRV088Nx4RITltWrxYuPz9rgil9ESZb4UvAWufb5bTA1XXxUhggCgD5/Te7fAGhHxoK13ThiJIRTLrDboH1SLztJkqN1aicBU1jENX3SumrQzzdntXekR8PtFVzgSJKnIP12DdtmAct6cJNZR7mkyYpiWqO0/XMPWDz3IIymR5MxlZECMzfL2ws+RpX2TRNkosmFnIkEItsovujIbDOEAZ8PV1SkQGiBzIKGT+mtdpjd3mFOMyqSjoCuQrRaPT0t3myZbJ+Qy5SqsT9KiPEybSwrGagAlzG/UER7/D552pvI2aXXZjTrBCDmfqkU2zRVlz2t6aOfZJxehZKUoitT7GjNM+APDZtBQcPSaJoeVjam19xbEmUNTm6c/eRckFny9CJA2BM1reeSdab435xZGi28xJKhkk7d+EBMuOBD8T7VPv8HR8VqA0oAyzMR20Q+6dneV2J54rEIcG0UJMNIIfu7ZfFh0/CHWfCB5CBmLAbdYbSr6uEkA1RpHA77t6RwaIHAyYKBD51OXblxwcjyqk+AFOjtPR24QfW1eJ90FGToMwrt8tFrBfLGqSOf+FclFGz/FIXX4Vv7Ib52LCWFLsGo8EipJYAyrUJ7gr9pi970/u3txhO2A9O6kCG7tB/B84w5e47Q1N9HK9pwi7VZ/qHFIF/vuexuJEn5ohTiP/2pnzfoPnz7qA8U+Go7GXX5pSL2JQBQ8C+d8l3ZIqhSe4wDaO6JZw+aenWbyRwLYndzCgiGSUaStgvhHL/OeqWspU+cxCZSnLazeUCWx4E/b+9ETo0ry+DJPY6xHpohHCfqmW2TZDKfxEZyYh6EbZfP/8rCTaFEI1cwpzPYbNzDTqSANuJELPFiH6Ez9sFxLLA3voVHtQ6kAKEl59kQY6/fa211bPzyHV7ACbV8XfNrbZ3uWzfSxJ4sEunRAUna95nnRJpB+3rz16heD/Tyh+YMg=='
-    let profile = Decrypter(rawMetadata.encryptedProfile);
-    console.log('-- decrypted char profile --', profile);
-    // TODO: decrypt profile
-    // let profile: CharacterProfile = {
-    //   name: metadata.name,
-      
-    // }
-    return rawMetadata;
+    let rawData = await firstValueFrom(this.http.get<any>(url));
+    console.log('meta resp', rawData);
+    let profile: CharacterProfile = JSON.parse(Decrypter(rawData.encryptedProfile));
+    let metadata: CharacterMetadata = {
+      name:rawData.name,
+      description: rawData.description,
+      rarity: rawData.rarity,
+      profile:profile,
+      image: rawData.endpoint
+    }
+    console.log('-- decrypted char meta --', metadata);
+    return metadata;
   }
   private _mapCatalogueData(res: any) : CatalogueCollection[]{
     return res.map((item:any) => { 
