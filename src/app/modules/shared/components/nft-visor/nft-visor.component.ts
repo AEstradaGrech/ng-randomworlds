2fc95261f7c569b21acaec90348a3dfceacfaa63
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-04-07 19:14:50 +0200

    [shared] --> adding RoundedButtonComponent. char-selection comp example OK

---
+++ src/app/modules/shared/components/nft-visor/nft-visor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-visor',
+  templateUrl: './nft-visor.component.html',
+  styleUrl: './nft-visor.component.scss'
+})
+export class NftVisorComponent {
+
+}
