9ee97538833259b9652e7356827dc0a98c2b011d
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-04-07 22:09:29 +0200

    [shared] --> nft-card OK. Display in char-selector OK. card action button bindings OK

--- src/app/modules/shared/components/nft-card/nft-card.component.ts
+++ src/app/modules/shared/components/nft-card/nft-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { AssetModel } from 'src/app/core/interfaces/business/smart-contract.interface';
+import { NftCardClickAction, RoundedButtonConfig } from '../../models/common-interfaces';
 
 @Component({
   selector: 'app-nft-card',
@@ -9,18 +10,28 @@ import { AssetModel } from 'src/app/core/interfaces/business/smart-contract.inte
 export class NftCardComponent implements OnInit {
 
   @Input() model!: AssetModel;
-
+  @Input() disabled: boolean = false;
+  @Input() cardButtons: RoundedButtonConfig[] = [];
+  @Output() onCardButtonClick: EventEmitter<NftCardClickAction> = new EventEmitter<NftCardClickAction>();
   ngOnInit(): void {
-    
+    if(this.cardButtons.length > 0){
+      this.cardButtons.forEach(btn => {
+        if(btn.id === undefined)
+          btn.id = btn.iconName;
+      });
+    }
   }
   getCollectionLogo(): string {
-    return this.model ? `url(${this.model.collectionLogoUrl}` : `url(assets/images/ng-app-logo.png)`;
+    return this.model ? this.model.collectionLogoUrl : `url(assets/images/ng-app-logo.png)`;
   }
-
+  onButtonClick(event:string){
+    console.log('-- clicked button ID --', event)
+    this.onCardButtonClick.emit({name:event, asset: this.model});
+  }
   onViewClick(){
 
   }
   onSelectClick(){
-    
+
   }
 }
