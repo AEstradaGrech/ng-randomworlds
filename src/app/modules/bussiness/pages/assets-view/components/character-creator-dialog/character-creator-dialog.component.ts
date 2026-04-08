73b8917d84d7c91479b356dc911756cb925392b9
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-04-08 20:39:04 +0200

    [assets-view] --> adding visualization type button. row | grid display OK

---
+++ src/app/modules/bussiness/pages/assets-view/components/character-creator-dialog/character-creator-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-creator-dialog',
+  templateUrl: './character-creator-dialog.component.html',
+  styleUrl: './character-creator-dialog.component.scss'
+})
+export class CharacterCreatorDialogComponent {
+
+}
