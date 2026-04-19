be06d3d9fcd604ba370de75fb7b6987b246701f6
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-04-20 00:47:31 +0200

    [char-creator] --> adding backend ambiences. updating styles

--- src/app/modules/bussiness/pages/assets-view/components/character-creator-dialog/character-creator-dialog.component.ts
+++ src/app/modules/bussiness/pages/assets-view/components/character-creator-dialog/character-creator-dialog.component.ts
@@ -1,8 +1,11 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { map } from 'rxjs/operators';
+import { ImagesService } from 'src/app/modules/bussiness/services/images.service';
 import { BaseComponent } from 'src/app/modules/shared/components/base.component';
 import { ESnackAlertType } from 'src/app/modules/shared/models/common-enums';
+import { SystemMessageDto } from 'src/app/modules/shared/models/mgmt-interfaces';
 
 @Component({
   selector: 'app-character-creator-dialog',
@@ -15,13 +18,25 @@ export class CharacterCreatorDialogComponent extends BaseComponent implements On
   
   availableAmbiences:string[] = ['Test 1','Test 2', 'Test 3'];
   selectedAmbiences:string[] = [];
-  
+  availableMoods:string[] = ['Test 1','Test 2', 'Test 3'];
+  selectedMoods:string[] = [];
   private _connectedWallet!: string;
+  private _imagesService: ImagesService = inject(ImagesService);
   ngOnInit(): void {
     this._connectedWallet = this.data.connectedWallet;
     if(!this._connectedWallet){
       this._notificationsService.openSnack(ESnackAlertType.WARN, "No connected wallet found, mint service unavailable");
     }
+    this._imagesService.getAmbiences().subscribe(res => {
+      console.log('-- on ambiences response --', res);
+      this.availableAmbiences = res.data.map((item:SystemMessageDto) => item.description);
+      console.log('-- mapped ambiences --', this.availableAmbiences);
+    })
+    this._imagesService.getAmbiences().subscribe(res => {
+      console.log('-- on moods response --', res);
+      this.availableMoods = res.data.map((item:SystemMessageDto) => item.description);
+      console.log('-- mapped moods --', this.availableMoods);
+    })
   }
 
   drop(event: CdkDragDrop<string[]>) {
