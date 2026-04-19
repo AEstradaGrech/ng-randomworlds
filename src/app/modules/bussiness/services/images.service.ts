5b1760650707d7d3a6e95fd8060a0565caf16b94
AEstradaGrech <alvaro.estrada89@gmail.com>
2026-04-20 00:47:31 +0200

    [char-creator] --> adding backend ambiences. updating styles

--- src/app/modules/bussiness/services/images.service.ts
+++ src/app/modules/bussiness/services/images.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GeneratedImageDto, GenerateImageResponse, GenerateImageRequest } from '../../shared/models/images.interfaces';
 import { environment } from 'src/environments/environment';
+import { CollectionResponse, QueryFilter } from '../../shared/models/common-interfaces';
+import { SystemMessageDto } from '../../shared/models/mgmt-interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +12,24 @@ import { environment } from 'src/environments/environment';
 export class ImagesService {
 
   // private _baseUrl:string = `${environment.baseUrl}/images`;
-  private _baseUrl:string = 'http://localhost:9000/randomworlds'
+  private _baseUrl:string = 'http://localhost:8080/images';
+  private _mgmtUrl: string = 'http://localhost:9000/randomworlds/mgmt/sys-messages';
+
   constructor(private http:HttpClient) { }
 
   generate(req: GenerateImageRequest) : Observable<GenerateImageResponse[]>{
     return this.http.post<any>(`${this._baseUrl}/generate/prompt`, req)
   }
-  apiInference(req: GenerateImageRequest) : Observable<GenerateImageResponse>{
-    return this.http.post<any>(`${this._baseUrl}/generate/inference-api/prompt`, req)
-  }
+
   getLastWithName(name:string) : Observable<GeneratedImageDto>{
-    return this.http.get<GeneratedImageDto>(`${this._baseUrl}/last/by-name/${name}`)
+    return this.http.get<GeneratedImageDto>(`${this._mgmtUrl}/last/by-name/${name}`)
+  }
+
+  getAmbiences(filter: QueryFilter | null = null) : Observable<CollectionResponse<SystemMessageDto>>{  
+    return this.http.post<CollectionResponse<SystemMessageDto>>(`${this._mgmtUrl}/query`, filter ?? { conditions: [{field:'tag', value:'ambience'}]});
+  }
+  
+  getMoods(filter: QueryFilter | null) : Observable<CollectionResponse<SystemMessageDto>>{
+    return this.http.post<CollectionResponse<SystemMessageDto>>(`${this._mgmtUrl}/query`, filter ?? { conditions: [{field:'tag', value:'mood'}]});
   }
 }
